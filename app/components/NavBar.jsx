fe19e28f9fa43e78be945fe9cd2395f6b7aa6d1c
arnaldo16763695 <arnaldoespinoza1@hotmail.com>
2023-11-03 13:21:39 -0400

    some mofifing in navBar

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -13,7 +13,7 @@ const NavBar = () => {
 
     return (
         <nav className="bg-gray-800 w-full">
-            <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div className="mx-auto w-full px-2 sm:px-6 lg:px-8">
                 <div className="relative flex h-16 items-center justify-between">
                     <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                         {/* Mobile menu button*/}
@@ -67,13 +67,13 @@ const NavBar = () => {
                         </button>
                     </div>
                     <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                        <div className="flex flex-shrink-0 items-center">
+                        <div className="flex flex-shrink-0 items-center ">
                             <Logo />
                         </div>
 
 
 
-                        <div className="hidden sm:ml-6 sm:block w-full">
+                        <div className="hidden sm:ml-6 sm:block w-full ">
                             <div className="flex space-x-4 justify-end text-right">
                                 {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
                                 <Link
